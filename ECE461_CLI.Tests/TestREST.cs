f700c50b6a7b758c1bc90585da17d229c76a89ef
Bazim <you@example.com>
2023-01-29 19:23:00 -0500

    Started NUnit Test Suite

---
+++ ECE461_CLI.Tests/TestREST.cs
@@ -0,0 +1,15 @@
+namespace ECE461_CLI.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.AreEqual("Bazim", "Bazim");
+    }
+}
