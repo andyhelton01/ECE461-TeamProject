1e4a31eb17c77799f543150d6eab5a552bb582fc
Bazim <you@example.com>
2023-01-31 15:37:42 -0500

    Added Calculate for ResponsiveMaintainer

--- ECE461_CLI/Metric.cs
+++ ECE461_CLI/Metric.cs
@@ -1,4 +1,9 @@
-﻿namespace ECE461_CLI
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace ECE461_CLI
 {
 	public abstract class Metric
 	{
@@ -54,5 +59,32 @@
 		}
 	}
 
+    public class ResponsiveMaintainer : Metric
+    {
 
+        public ResponsiveMaintainer(Library parentLibrary) : base(parentLibrary)
+        {
+            this.weight = 0;
+            this.name = "ResponsiveMaintainer";
+        }
+
+
+        public override async void Calculate()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("my-cool-cli"));
+            var tokenAuth = new Credentials("token"); // Add environment variable for token
+            client.Credentials = tokenAuth;
+
+			// Get repo using information from Library (owner and name)
+			var repo = await client.Repository.Get("andyhelton01", "ECE461-TeamProject");
+            var commits = await client.Repository.Commit.GetAll(repo.Id);
+            var lastCommit = commits.FirstOrDefault();
+
+            var lastCommitDate = lastCommit.Commit.Author.Date;
+            var curDate = System.DateTimeOffset.Now;
+            var timeSinceLastCommit = curDate - lastCommitDate;
+
+			this.score = (float) Math.Exp(-timeSinceLastCommit.Days);
+        }
+    }
 }
