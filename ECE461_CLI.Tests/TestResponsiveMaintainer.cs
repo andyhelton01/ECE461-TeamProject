741119ba661d777326d379ae4aba1ce8b862d916
Bazim <you@example.com>
2023-02-03 13:47:28 -0500

    P created test coverage prototype

---
+++ ECE461_CLI.Tests/TestResponsiveMaintainer.cs
@@ -0,0 +1,23 @@
+﻿namespace ECE461_CLI.Tests;
+
+public class ResponsiveMaintainerTests
+{
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+
+    [Test]
+    public void TestResponsiveMaintainer()
+    {
+        Library lib = new GitUrlLibrary("https://github.com/andyhelton01/ECE461-TeamProject");
+        lib.GetScore();
+
+        foreach (Metric m in lib.metrics)
+        {
+            if (m.name == "ResponsiveMaintainer")
+                Assert.That(m.score, Is.GreaterThan(0.7));
+        }
+    }
+}
