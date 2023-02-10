4c32002ed7fb9d2d4104afbed20b7b71511ba620
Bazim <you@example.com>
2023-02-10 12:30:11 -0500

    added busfactor tests

---
+++ ECE461_CLI.Tests/TestBusFactor.cs
@@ -0,0 +1,29 @@
+﻿namespace ECE461_CLI.Tests;
+
+public class BusFactorTests
+{
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+
+    [Test]
+    public async Task TestBusFactorHighScore()
+    {
+        GitUrlLibrary lib = new GitUrlLibrary("https://github.com/pytorch/pytorch");
+        Metric m = new BusFactor(lib);
+        await m.Calculate();
+        Assert.That(m.score, Is.GreaterThan(0.7));
+    }
+
+    [Test]
+    public async Task TestBusFactorLowScore()
+    {
+        GitUrlLibrary lib = new GitUrlLibrary("https://github.com/skejserjensen/Hangman");
+        Metric m = new BusFactor(lib);
+        await m.Calculate();
+        Assert.That(m.score, Is.LessThan(0.5));
+    }
+
+}
